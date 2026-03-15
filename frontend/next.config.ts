bd4f7763b155d5a3b5af14c2736d3d9dc04dafe8
MohamedMagdy <ymmagdyfofo@gmail.com>
2026-03-15 07:48:50 +0200

    added turpopack config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -16,6 +16,8 @@ const nextConfig: NextConfig = {
     }
     return config;
   },
+  // Ensure we can build despite the webpack config under Turbopack
+  turbopack: {},
 };
 
 export default nextConfig;
